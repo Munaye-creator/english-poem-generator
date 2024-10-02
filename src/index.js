50e55b8d6dc0edfa0a7cd2816201f0bdf7aa4da4
Twins-coder <jocelynnn3@gmail.com>
2024-10-02 17:12:53 +0200

    Removed the console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response){
-    console.log("poem generated");
     new Typewriter("#poem", {
   strings: response.data.answer,
   autoStart: true,
@@ -27,9 +26,7 @@ function generatePoem(event){
     poemElement.classList.remove("hidden");
     poemElement.innerHTML=`<div class="generating"> ⏳Generating poem about ${instructionsInput.value}</div>`;
 
-    console.log("Generating poem");
-    console.log(`Prompt:${prompt}`);
-    console.log(`Context:${context}`);
+    
   
 axios.get(apiUrl).then(displayPoem);
 
